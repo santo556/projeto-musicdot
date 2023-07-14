29fb0eb31236854e53aa9c5558053767f52fa898
André Venâncio dos Santos <72739630+santo556@users.noreply.github.com>
2023-07-13 21:49:05 -0400

    Testes.

---
+++ _script/funcoes.js
@@ -0,0 +1,6 @@
+/* Conversões */
+var textoInteiro = "10"
+var inteiro = parseInt(textoInteiro)
+
+var textoFloat = "10.22"
+var float = parseFloat(textoFloat)
---
+++ _script/hello.js
@@ -0,0 +1,9 @@
+//alert("Olá, Novo Mundo!")
+/*var mensagem = "Olá, Novo Mundo!"
+console.log(mensagem)*/
+
+//var titulo = document.querySelector("h1")
+//titulo.textContent = "Novo titulo"
+function mostraAlert() {
+    alert("Funciona!")
+}
