f0c98e89f6ad0b524f05f09a205a4d6b6431fabe
André Venâncio dos Santos <72739630+santo556@users.noreply.github.com>
2023-07-14 18:14:46 -0400

    Removido

--- _script/funcoes.js
+++
@@ -1,6 +0,0 @@
-/* Conversões */
-var textoInteiro = "10"
-var inteiro = parseInt(textoInteiro)
-
-var textoFloat = "10.22"
-var float = parseFloat(textoFloat)
--- _script/hello.js
+++
@@ -1,9 +0,0 @@
-//alert("Olá, Novo Mundo!")
-/*var mensagem = "Olá, Novo Mundo!"
-console.log(mensagem)*/
-
-//var titulo = document.querySelector("h1")
-//titulo.textContent = "Novo titulo"
-function mostraAlert() {
-    alert("Funciona!")
-}
